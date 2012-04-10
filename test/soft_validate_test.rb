c6b346fd555169165befb84608d2cc6e3e5b2ef7
Jason Vasquez <jason.vasquez@developertown.com>
2012-04-10 16:58:49 -0400

    Removing dummy app

--- test/soft_validate_test.rb
+++ test/soft_validate_test.rb
@@ -4,4 +4,8 @@ class SoftValidateTest < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, SoftValidate
   end
+
+  test "a soft validated record will have a #soft_valid?" do
+    
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,7 @@
-# Configure Rails Environment
-ENV["RAILS_ENV"] = "test"
+$:.unshift(File.dirname(__FILE__) + '/../lib')
 
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-require "rails/test_help"
 
-Rails.backtrace_cleaner.remove_silencers!
-
-# Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
-# Load fixtures from the engine
-if ActiveSupport::TestCase.method_defined?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
-end
+require 'rubygems'
+require 'test/unit'
+require 'active_record'
+require 'active_record/fixtures'
